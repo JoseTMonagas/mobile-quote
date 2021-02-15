d41fc904730c629dabb380c1605a2b4866431c7d
Jose T. Monagas <jmonagas@localhost.localdomain>
2021-02-15 16:43:26 -0300

    Create Device model with all options

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Devices/Index', [
+            'devices' => Device::all()->map(function (Device $device) {
+                    return [
+                        'id' => $device->id,
+                        'name' => $device->name,
+                        'company' => $device->company,
+                        'edit_url' => route('device.edit', $device)
+                    ];
+                }),
+            'create_url' => route('device.create'),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Device $device)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Device $device)
+    {
+        //
+    }
+}
