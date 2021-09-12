63f0f7e839d94de2bdbe2e3b6e6b0e552cc7c5c7
Jose T. Monagas <jose_tadeomong@hotmail.com>
2021-09-12 18:08:08 -0300

    Add overflow scrolling to report table

--- resources/js/Pages/Reports/Show.vue
+++ resources/js/Pages/Reports/Show.vue
@@ -45,7 +45,7 @@
 
         <div class="py-6" v-if="report.length > 0">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="bg-white overflow-x-scroll shadow-xl sm:rounded-lg">
                     <button
                         class="shadow bg-blue-300 py-1 px-2"
                         @click="onClickExport"
