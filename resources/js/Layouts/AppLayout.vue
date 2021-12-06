7acc7d0d64da8d45d0a67f54d2e08f00144c7c28
Jose Monagas <jose_tadeomong@hotmail.com>
2021-12-06 14:58:52 -0300

    refactor: Remove teams from AppLayout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -58,7 +58,7 @@
                                 </jet-nav-link>
                                 <jet-nav-link
                                     v-if="
-                                        $page.props.user.role == 'ADMIN' &&
+                                        userRole == 'ADMIN' &&
                                             $page.props.user.store_id > 0
                                     "
                                     :href="
@@ -111,189 +111,9 @@
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <div class="ml-3 relative">
-                                <!-- Teams Dropdown -->
-                                <jet-dropdown
-                                    align="right"
-                                    width="60"
-                                    v-if="$page.props.jetstream.hasTeamFeatures"
-                                >
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150"
-                                            >
-                                                {{
-                                                    $page.props.user
-                                                        .current_team.name
-                                                }}
-
-                                                <svg
-                                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <div class="w-60">
-                                            <!-- Team Management -->
-                                            <template
-                                                v-if="
-                                                    $page.props.jetstream
-                                                        .hasTeamFeatures
-                                                "
-                                            >
-                                                <div
-                                                    class="block px-4 py-2 text-xs text-gray-400"
-                                                >
-                                                    Manage Team
-                                                </div>
-
-                                                <!-- Team Settings -->
-                                                <jet-dropdown-link
-                                                    :href="
-                                                        route(
-                                                            'teams.show',
-                                                            $page.props.user
-                                                                .current_team
-                                                        )
-                                                    "
-                                                >
-                                                    Team Settings
-                                                </jet-dropdown-link>
-
-                                                <jet-dropdown-link
-                                                    :href="
-                                                        route('teams.create')
-                                                    "
-                                                    v-if="
-                                                        $page.props.jetstream
-                                                            .canCreateTeams
-                                                    "
-                                                >
-                                                    Create New Team
-                                                </jet-dropdown-link>
-
-                                                <div
-                                                    class="border-t border-gray-100"
-                                                ></div>
-
-                                                <!-- Team Switcher -->
-                                                <div
-                                                    class="block px-4 py-2 text-xs text-gray-400"
-                                                >
-                                                    Switch Teams
-                                                </div>
-
-                                                <template
-                                                    v-for="team in $page.props
-                                                        .user.all_teams"
-                                                >
-                                                    <form
-                                                        @submit.prevent="
-                                                            switchToTeam(team)
-                                                        "
-                                                        :key="team.id"
-                                                    >
-                                                        <jet-dropdown-link
-                                                            as="button"
-                                                        >
-                                                            <div
-                                                                class="flex items-center"
-                                                            >
-                                                                <svg
-                                                                    v-if="
-                                                                        team.id ==
-                                                                            $page
-                                                                                .props
-                                                                                .user
-                                                                                .current_team_id
-                                                                    "
-                                                                    class="mr-2 h-5 w-5 text-green-400"
-                                                                    fill="none"
-                                                                    stroke-linecap="round"
-                                                                    stroke-linejoin="round"
-                                                                    stroke-width="2"
-                                                                    stroke="currentColor"
-                                                                    viewBox="0 0 24 24"
-                                                                >
-                                                                    <path
-                                                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                                    ></path>
-                                                                </svg>
-                                                                <div>
-                                                                    {{
-                                                                        team.name
-                                                                    }}
-                                                                </div>
-                                                            </div>
-                                                        </jet-dropdown-link>
-                                                    </form>
-                                                </template>
-                                            </template>
-                                        </div>
-                                    </template>
-                                </jet-dropdown>
-                            </div>
-
                             <!-- Settings Dropdown -->
                             <div class="ml-3 relative">
                                 <jet-dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <button
-                                            v-if="
-                                                $page.props.jetstream
-                                                    .managesProfilePhotos
-                                            "
-                                            class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out"
-                                        >
-                                            <img
-                                                class="h-8 w-8 rounded-full object-cover"
-                                                :src="
-                                                    $page.props.user
-                                                        .profile_photo_url
-                                                "
-                                                :alt="$page.props.user.name"
-                                            />
-                                        </button>
-
-                                        <span
-                                            v-else
-                                            class="inline-flex rounded-md"
-                                        >
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {{ $page.props.user.name }}
-
-                                                <svg
-                                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
                                     <template #content>
                                         <!-- Account Management -->
                                         <div
@@ -421,7 +241,7 @@
                         </jet-responsive-nav-link>
                         <jet-responsive-nav-link
                             v-if="
-                                $page.props.user.role == 'ADMIN' &&
+                                userRole == 'ADMIN' &&
                                     $page.props.user.store_id > 0
                             "
                             :href="
@@ -633,7 +453,10 @@ export default {
 
     computed: {
         userRole() {
-            return this.$page.props.user.role;
+            if (this.$page.props.user) {
+                return this.$page.props.user.role;
+            }
+            return "NONE";
         }
     },
 
